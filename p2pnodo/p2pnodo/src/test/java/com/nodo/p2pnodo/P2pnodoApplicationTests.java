eedbec107534c4842a3d8ecde7f05b3f4aa052cb
Aaron <aaronrodrigo@hotmail.com>
2025-08-07 06:52:39 -0600

    el dockeeeeeer

--- p2pnodo/p2pnodo/src/test/java/com/nodo/p2pnodo/P2pnodoApplicationTests.java
+++ p2pnodo/p2pnodo/src/test/java/com/nodo/p2pnodo/P2pnodoApplicationTests.java
@@ -1,29 +1,35 @@
 package com.nodo.p2pnodo;
 
+
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest(classes = TestRedisConfig.class)
+@SpringBootTest
+@ActiveProfiles("test")
 class P2pnodoApplicationTests {
 
     @Test
     void contextLoads() {
-        // Test de carga de contexto
+        // Test vacío solo para verificar carga del contexto
+    }
+
+    @Configuration
+    static class TestConfig {
+        @Bean
+        public RedisConnectionFactory redisConnectionFactory() {
+            LettuceConnectionFactory factory = new LettuceConnectionFactory();
+            factory.setHostName("localhost");
+            factory.setPort(6379);
+            factory.setDatabase(0);
+            factory.afterPropertiesSet();
+            return factory;
+        }
     }
 }
 
-@TestConfiguration
-class TestRedisConfig {
-    @Bean
-    public RedisConnectionFactory redisConnectionFactory() {
-        LettuceConnectionFactory factory = new LettuceConnectionFactory();
-        factory.setHostName("localhost");
-        factory.setPort(6379);
-        factory.afterPropertiesSet();
-        return factory;
-    }
-}
